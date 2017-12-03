6267c7beb6474ae910f20cb3f3a46ea63a680365
Robert Campbell <robertgcampbell95@gmail.com>
2017-12-02 23:15:04 -0800

    Fix typo in app.js

--- arteshop/app.js
+++ arteshop/app.js
@@ -49,7 +49,7 @@ app.get('/', (req, res) => {
 });
 
 app.get('*', (req, res) => {
-  res.sendFile(path.join(_dirname, 'public/index.html'));
+  res.sendFile(path.join(__dirname, 'public/index.html'));
 });
 
 //Start Server
