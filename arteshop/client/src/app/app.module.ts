9582b9b8bfbc4493ae8f39070f6f73f58fbe52b4
Robert Campbell <robertgcampbell95@gmail.com>
2017-12-02 23:07:31 -0800

    Test deployment bug fix

--- arteshop/client/src/app/app.module.ts
+++ arteshop/client/src/app/app.module.ts
@@ -22,7 +22,8 @@ const appRoutes: Routes =  [
   {path:'register', component: RegisterComponent},
   {path:'login', component: LoginComponent},
   {path:'dashboard', component: DashboardComponent, canActivate:[AuthGuard]},
-  {path:'profile', component: ProfileComponent}
+  {path:'profile', component: ProfileComponent},
+  {path:'navbar', component: NavbarComponent}
 ]
 
 @NgModule({
