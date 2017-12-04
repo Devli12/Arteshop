d595b7735c60daa0bf916ca288843ee4b390cf56
Robert Campbell <robertgcampbell95@gmail.com>
2017-12-04 03:55:40 -0800

    Add default image to profile pic

--- arteshop/client/src/app/components/profile/profile.component.ts
+++ arteshop/client/src/app/components/profile/profile.component.ts
@@ -11,6 +11,7 @@ import {Router} from '@angular/router';
 export class ProfileComponent implements OnInit {
   user:Object;
   profilePic: Object;
+  picUrl: String;
 
   constructor(private authService:AuthService, private router:Router) { }
 
@@ -22,17 +23,23 @@ export class ProfileComponent implements OnInit {
       console.log(err);
       return false;
     });
+	
+	this.getProfilePicture();
   }
   
+/*   
   addImage() {
-    // Save selected image 
-    // Save image path into database using HTTP post and backend scripting (Express?)
-  }
-  
+     Save selected image 
+     Save image path into database using HTTP post and backend scripting (Express?)
+  } 
+  */
   getProfilePicture() {
+	  
+	  var placeholderPath = '../../../assets/avatar_placeholder.png';
 	  // Attempt to fetch the image reference from the database using a service
 		// Return the image path
 	  // If the attempt returns null
 		// Return the placeholder image path
+	this.picUrl = placeholderPath;
   }
 }
