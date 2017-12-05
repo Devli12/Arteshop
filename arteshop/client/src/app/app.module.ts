687eacd1cf964219112c23f7569f0360b6cd8d3b
Robert Campbell <robertgcampbell95@gmail.com>
2017-12-05 11:52:34 -0800

    Add button to navigate to dashboard.

--- arteshop/client/src/app/app.module.ts
+++ arteshop/client/src/app/app.module.ts
@@ -24,7 +24,7 @@ const appRoutes: Routes =  [
   {path:'', component: HomeComponent},
   {path:'register', component: RegisterComponent},
   {path:'login', component: LoginComponent},
-  {path:'dashboard', component: DashboardComponent, canActivate:[AuthGuard]},
+  {path:'dashboard', component: DashboardComponent},
   {path:'profile', component: ProfileComponent},
   {path:'navbar', component: NavbarComponent},
   {path:'bio', component: BioComponent}
